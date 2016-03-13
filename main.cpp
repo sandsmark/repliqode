ace450691cbf4c472840981bd6b5c87b81947ef2
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2016-03-13 17:21:52 +0100

    invert application palette, so it looks better on my screen

--- main.cpp
+++ main.cpp
@@ -9,6 +9,20 @@ int main(int argc, char *argv[])
     application.setApplicationName("qhiveplot");
     application.setOrganizationDomain("nous.ai");
 
+    // Invert the palette, for reasons
+    QPalette palette = QApplication::palette();
+
+    for (int group = 0; group < QPalette::NColorGroups; group++) {
+        QPalette::ColorGroup colorGroup = QPalette::ColorGroup(group);
+        for (int role = 0; role < QPalette::NColorRoles; role++) {
+            QPalette::ColorRole colorRole = QPalette::ColorRole(role);
+            QColor color = palette.color(colorGroup, colorRole);
+            QColor inverted(255 - color.red(), 255 - color.green(), 255 - color.blue(), color.alpha());
+            palette.setColor(colorGroup, colorRole, inverted);
+        }
+    }
+    QApplication::setPalette(palette);
+
     // Fix antialiasing quality of hive plot
     QSurfaceFormat fmt;
     fmt.setSamples(32);
