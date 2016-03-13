07d1a830f50945bb6243fa77d4423b37654ca4a0
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2016-03-13 17:21:58 +0100

    show fullscreen

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     QSurfaceFormat::setDefaultFormat(fmt);
 
     Window window;
-    window.show();
+    window.showFullScreen();
 
     return application.exec();
 }
